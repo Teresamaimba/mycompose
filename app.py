8749a7b23b6311631ea85c64a687188680a26fa9
Teresamaimba <teresamaimba@gmail.com>
2026-04-19 16:44:19 +0300

    Fixed app.py syntax - interview ready

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from http.server import HTTPServer, BaseHTTPRequestHandler
+from http.server import BaseHTTPRequestHandler, HTTPServer
 
 class SimpleHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
         self.end_headers()
-        self.wfile.write(b"Hello from Teresia apps - version 2!!")
+        self.wfile.write(b"Hello from Teresia Maimba - DevOps Support Project!!")
 
 server = HTTPServer(("0.0.0.0", 8080), SimpleHandler)
 print("Server running on port 8080...")
